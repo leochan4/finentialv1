aeda84bc090bc380778001352c3c26c37b040af4
Leo Suen <lsuen30@gmail.com>
2025-03-23 20:31:58 -0700

    Initial Commit

---
+++ finentialv1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finentialv1
+//
+//  Created by Leo Suen on 2025-03-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
